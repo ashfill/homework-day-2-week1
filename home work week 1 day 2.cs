ada42a04f583bee0f6b77ac76a6f0b86ba88cb21
ashfill <ashtonfillery@gmail.com>
2016-09-27 12:10:57 -0700

    Add files via upload

---
+++ home work week 1 day 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+         for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0) Console.Write("fizz");
+                if (i % 5 == 0) Console.Write("buzz");
+                if (Console.CursorLeft == 0) Console.Write(i);
+
+
+                Console.WriteLine();
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
